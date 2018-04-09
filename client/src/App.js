7b4e96a25fe73da1f453f724f33373ec72084bfe
Byron <jamesblocker81@gmail.com>
2018-04-09 14:01:14 -0700

    add logout and signup views and functionality

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,41 @@ import './App.css';
 import httpClient from './httpClient'
 
 import LogIn from './views/LogIn'
+import LogOut from './views/LogOut'
+import SignUp from './views/SignUp'
 
 class App extends Component {
+
+  state = { currentUser: httpClient.getCurrentUser() }
+
+  onLoginSuccess(user) {
+    this.setState({ currentUser: httpClient.getCurrentUser() })
+  }
+
+  logOut() {
+    httpClient.logOut()
+    this.setState({ currentUser: null })
+  }
+
   render() {
     return (
       <div className="App container">
-        <h1>Hi</h1>
+        
+        <Switch>
+          <Route path="/login" render={(props) => {
+            return <LogIn {...props} onLoginSuccess={this.onLoginSuccess.bind(this)} />
+          }} />
+
+          <Route path="/logout" render={(props) => {
+            return <LogOut onLogOut={this.logOut.bind(this)}  />
+          }} />
+
+          <Route path="/signup" render={(props) => {
+            return <SignUp {...props} onSignUpSuccess={this.onLoginSuccess.bind(this)} />
+          }} />
+
+        </Switch>
+
       </div>
     );
   }
