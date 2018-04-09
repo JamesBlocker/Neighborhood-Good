dea7ad853b8e1dba350b14fd51ca99d4f51ffb39
Byron <jamesblocker81@gmail.com>
2018-04-09 10:58:24 -0700

    initial set up and user authentication

---
+++ controllers/users.js
@@ -0,0 +1,54 @@
+const User = require('../models/User.js')
+const signToken = require('../serverAuth.js').signToken
+
+module.exports = {
+
+    index: (req,res) => {
+        User.find({}, (err, users) => {
+            res.json(users)
+        })
+    },
+
+    show: (req, res) => {
+        console.log("Current user:")
+        console.log(req.user)
+        User.findById(req.params.id, (err, user) => {
+            res.json(user)
+        })
+    },
+
+    create: (req, res) => {
+        User.create(req.body, (err, user) => {
+            if(err) return res.json({ success: false, code: err.code })
+            const token = signToken(user)
+            res.json({ success: true, message: "User created - token attached" })
+        })
+    },
+
+    update: (req, res) => {
+        User.findById(req.params.id, (err, user) => {
+            Object.assign(user, req.body)
+            user.save((err, updatedUser) => {
+                res.json({ success: true, message: "User updated", user })
+            })
+        })
+    },
+
+    destroy: (req, res) => {
+        User.findByIdAndRemove(req.params.id, (err, user) => {
+            res.json({ success: true, message: "User deleted", user })
+        })
+    },
+
+    authenticate: (req, res) => {
+        User.findOne({ email: req.body.email }, (err, user) => {
+            if(!user || !user.validPassword(req.body.password)) {
+                return res.json({ success: false, message: "Invalid credential" })
+            }
+
+            const token = signToken(user)
+            res.json({ success: true, message: "Token attached", token })
+        })
+    }
+
+}
---
+++ server.js
@@ -0,0 +1,26 @@
+const
+    dotenv = require('dotenv').load(),
+    express = require('express'),
+    app = express(),
+    logger = require('morgan'),
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose'),
+    MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/react-express-jwt',
+    PORT = process.env.PORT || 3001
+    userRoutes = require('./routes/users.js'),
+    // postsRoutes = require('./routes/posts.js')
+
+mongoose.connect(MONGODB_URI, (err) => {
+    console.log(err || 'Connected to MongoDB')
+})
+
+// app.use(express.static(`${__dirname}/client/build`))
+app.use(logger('dev'))
+app.use(bodyParser.json())
+
+app.use('/api/users', userRoutes)
+// app.use('/api/posts', postsRoutes)
+
+app.listen(PORT, (err) => {
+    console.log(err || `Server running on port ${PORT}`)
+})
