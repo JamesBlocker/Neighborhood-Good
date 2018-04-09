f4b441a62747250609dfaf4490c8517ab2bbaf30
Byron <jamesblocker81@gmail.com>
2018-04-09 11:42:53 -0700

    add react framework and get running

--- server.js
+++ server.js
@@ -14,9 +14,13 @@ mongoose.connect(MONGODB_URI, (err) => {
     console.log(err || 'Connected to MongoDB')
 })
 
-// app.use(express.static(`${__dirname}/client/build`))
+app.use(express.static(`${__dirname}/client/build`))
 app.use(logger('dev'))
 app.use(bodyParser.json())
+
+app.get('/api', (req, res) => {
+    res.json({ message: "API root" })
+})
 
 app.use('/api/users', userRoutes)
 app.use('/api/posts', postsRoutes)
